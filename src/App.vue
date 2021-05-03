ae498ba6d3acc827f8875428180fc4fb461e74d9
1073898183@qq.com <1073898183@qq.com>
2021-05-03 23:49:04 +0900

    添加live2d

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,22 @@
 <script>
 export default {
   name: 'App',
+  setup () {
+    // 老婆
+    setTimeout(() => {
+      window.L2Dwidget.init({
+        pluginRootPath: '/live2dw/',
+        pluginJsPath: 'lib/',
+        pluginModelPath: 'live2d-widget-model-koharu/assets/',
+        tagMode: false,
+        debug: false,
+        model: { jsonPath: '/live2dw/live2d-widget-model-koharu/assets/koharu.model.json' },
+        display: { position: 'left', width: 250, height: 350 },
+        mobile: { show: true },
+        log: false
+      })
+    }, 1000)
+  },
   watch: {
     '$route' (to, from) {
       const toDepth = to.path.split('/').length
